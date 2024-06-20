eeb950d4ef1ba54245718b000515674551df03ac
Anas07git <anas.karatela@gmail.com>
2024-06-20 12:44:21 +0530

    First Commit

---
+++ Task-1 ( Hello World )/index.js
@@ -0,0 +1,10 @@
+// 3 diffrent ways to print Hello World in javascript
+
+// 1) console
+console.log("Hello World")
+
+// 2) alert
+alert("Hello World")
+
+// 3) document.write()
+document.write("Hello World")
